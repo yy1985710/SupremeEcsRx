0b66adf89960812de86de0e88b4f6e9b765b24a9
yy1985710 <450130209@qq.com>
2018-09-12 15:53:42 +0800

    add module

---
+++ SupremeEcsRx/Assets/Framework/Modules/NetworkModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EcsRx.Infrastructure.Dependencies;
+using EcsRx.Network;
+using EcsRx.Unity.Network;
+
+namespace EcsRx.Unity.Modules
+{
+    public class NetworkModule : IDependencyModule
+    {
+        public void Setup(IDependencyContainer container)
+        {
+            container.Bind<HttpManager>();
+            container.Bind<SocketManager>();
+            container.Bind<IHttpProtocol, DefaultHttpProtocol>();
+            container.Bind<ISocketProtocol, SocketProtocol>();
+        }
+    }
+}
---
+++ SupremeEcsRx/Assets/Framework/Modules/ResourceLoaderModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EcsRx.Infrastructure.Dependencies;
+using EcsRx.Unity.Loader;
+
+namespace EcsRx.Unity.Modules
+{
+    public class ResourceLoaderModule : IDependencyModule
+    {
+        public void Setup(IDependencyContainer container)
+        {
+            container.Bind<ILoadStrategy, LocalPriorLoadStrategy>();
+            container.Bind<LocalFileLoader>();
+            container.Bind<RemoteFileLoader>();
+            container.Bind<ImageLoader>();
+            container.Bind<IResourceLoader, ResourceLoader>();
+            container.Bind<IResourceLoader, AssetBundleLoader>(new BindingConfiguration { WithName = "AssetBundle" });
+        }
+    }
+}
